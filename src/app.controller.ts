8a22ff9ce44f25566fae0510a32c74713b178002
jantapbh <s6035512034@phuket.psu.ac.th>
2020-09-18 18:03:37 +0700

    add commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 1 Controller แทนด้วย route 1 ชุด
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() //Get Method
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+
+  
 }
