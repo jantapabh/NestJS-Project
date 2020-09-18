5d6113ea707dfc67d46648e1a6abc0fe3bdb18f7
jantapbh <s6035512034@phuket.psu.ac.th>
2020-09-18 18:53:24 +0700

    add commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { from } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-
+import {TodoService} from './todo/todo.service';
+ 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+
+
+    @Get()
+    getTodos(){
+        return [1,2,3]
+    }
+
+    @Post()
+    postTodo(@Body("title") title:string , @Body("subtitle") subtitle:string)
+    {
+
+        console.log(`title: ${title}, subtitle: ${subtitle}`);
+    }
+}
