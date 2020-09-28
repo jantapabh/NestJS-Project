77fc5a7d81ae65e4500e1d0400147ee612697826
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-28 21:30:28 +0700

    add commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { from } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TodoService} from './todo/todo.service';
-import {TodoController} from './todo/todo.controller';
-import { GuestsModule } from './guests/guests.module';
+
 
 
 @Module({
   imports: [GuestsModule],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
