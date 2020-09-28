462d20d33993c8e572742b307756dca2dae5b257
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-28 21:23:42 +0700

    add commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TodoService} from './todo/todo.service';
 import {TodoController} from './todo/todo.controller';
+import { GuestsModule } from './guests/guests.module';
 
 
 @Module({
-  imports: [],
+  imports: [GuestsModule],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
--- src/todo/todo.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodoService } from './todo.service';
-
-@Module({
-  providers: [TodoService]
-})
-export class TodoModule {}
